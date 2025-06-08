21eb747f7b17d6d05874532107c69f51929af803
Arghonaut <lo.rivemale@gmail.com>
2025-06-08 13:20:50 +0700

    Quick turbulence / Displace update

--- Operator/Turbulence.cs
+++ Operator/Turbulence.cs
@@ -157,8 +157,8 @@ namespace LibNoise.Operator
             _materialGPU = XNoiseShaderCache.GetMaterial(XNoiseShaderPaths.Turbulence);
 
             var tmpTurbulence = renderingDatas.displacementMap;
-            Vector3 tmpTranslate = renderingDatas.origin;
-            //_materialGPU.SetTexture("_TextureA", Modules[0].GetValueGPU(renderingDatas));
+            Vector3 tmpOrigin = renderingDatas.origin;
+            _materialGPU.SetTexture("_OriginalDisplacementMap", renderingDatas.displacementMap);
             renderingDatas.origin = new Vector3((float)X0, (float)Y0, (float)Z0);
             _materialGPU.SetTexture("_PerlinA", new Perlin().GetValueGPU(renderingDatas));
             renderingDatas.origin = new Vector3((float)X1, (float)Y1, (float)Z1);
@@ -166,8 +166,11 @@ namespace LibNoise.Operator
             renderingDatas.origin = new Vector3((float)X2, (float)Y2, (float)Z2);
             _materialGPU.SetTexture("_PerlinC", new Perlin().GetValueGPU(renderingDatas));
             _materialGPU.SetFloat("_Power", (float)Power);
-            renderingDatas.origin = tmpTranslate;
+            renderingDatas.origin = tmpOrigin;
+
+            ImageFileHelpers.SaveToJPG(ImageFileHelpers.toTexture2D(renderingDatas.displacementMap), "/", "TURBULENCE_BEFORE");
             renderingDatas.displacementMap = GetImage(_materialGPU, renderingDatas);
+            ImageFileHelpers.SaveToJPG(ImageFileHelpers.toTexture2D(renderingDatas.displacementMap), "/", "TURBULENCE_AFTER");
 
             var value = Modules[0].GetValueGPU(renderingDatas);
             //renderingDatas.displacementMap = tmpTurbulence;
